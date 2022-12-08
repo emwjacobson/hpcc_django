143b013dd51f548efca826bf134bdf715c4406b0
Emerson Jacobson <emwjacobson@gmail.com>
2022-12-07 18:10:27 -0800

    Made basic initial models and migrations

--- hpcc/address_book/models.py
+++ hpcc/address_book/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# User
+#   First Name
+#   Last Name
+#   Address
+#   Phone Number
+
+
+# Address
+#   Number
+#   Street Name
+#   Apt Number (Optional)
+#   Zip code
+#   City
+#   State
+
+class Address(models.Model):
+    number = models.CharField(max_length=8)
+    street_name = models.CharField(max_length=64)
+    apt = models.CharField(max_length=64, default='')
+    zip_code = models.IntegerField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=32)
+
+class User(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=16)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    
--- hpcc/hpcc/settings.py
+++ hpcc/hpcc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'address_book.apps.AddressBookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
